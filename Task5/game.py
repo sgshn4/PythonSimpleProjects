780775f42e5f487ff25de9d4310b45579993c188
sgshn4 <davidov.oleg2018@yandex.ru>
2024-03-18 21:39:27 +0300

    Added map randomizer, map visualization.

--- Task5/game.py
+++ Task5/game.py
@@ -1,3 +1,39 @@
+import random
+import copy
+
+
 class Game:
-    def __init__(self):
-        pass
+    def __init__(self, w, h):
+        self.map_w = w
+        self.map_h = h
+        self.x = random.randint(0, w)
+        self.y = random.randint(0, h)
+        self.game_map = self.randomize_map()
+
+    def randomize_map(self):
+        result = []
+        for i in range(self.map_h):
+            result.append([])
+            for j in range(self.map_w):
+                if i == self.y and  j == self.x:
+                    result[i].append('*')
+                else:
+                    result[i].append(random.randint(1, 6))
+        return result
+
+    def make_move(self, point_x, point_y):
+        # Move on Y Axis
+        if point_x == self.x:
+            for i in range(self.game_map[point_y][point_x]):
+                if 0 < self.y + self.y - point_y < self.map_h and self.game_map[self.y + self.y - point_y][self.x] is not '*':
+                    self.game_map[self.y][self.x] = '*'
+                    self.y + self.y - point_y
+        elif point_y == self.y:
+            # Move on X Axis
+            for i in range(self.game_map[point_y][point_x]):
+                if 0 < self.x + self.x - point_x < self.map_w and self.game_map[self.y][self.x + self.x - point_x] is not '*':
+                    self.game_map[self.y][self.x] = '*'
+                    self.x + self.x - point_x
+
+
+
--- Task5/gui.py
+++ Task5/gui.py
@@ -1,4 +1,8 @@
-from PyQt5.QtWidgets import QWidget, QDesktopWidget, QPushButton, QListWidget, QHBoxLayout, QVBoxLayout, QLabel
+from PyQt5.QtWidgets import QWidget, QDesktopWidget, QPushButton, QTableWidget, QHBoxLayout, QVBoxLayout, QLabel
+from PyQt5.QtWidgets import QTableWidgetItem
+from PyQt5.QtGui import QColor
+import game
+
 
 class MainWindow(QWidget):
     def __init__(self):
@@ -7,11 +11,19 @@ class MainWindow(QWidget):
 
     def init_ui(self):
         self.resize(1280, 720)
-        self.center()
+        self.game = game.Game(10, 10)
+        self.game.randomize_map()
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
         self.setWindowTitle('Spy Game')
 
         hbox = QHBoxLayout()
-        list_widget = QListWidget()
+        self.table_widget = QTableWidget()
+        self.table_widget.setColumnCount(self.game.map_w)
+        self.table_widget.setRowCount(self.game.map_h)
+        self.update_table()
         vbox = QVBoxLayout()
         game_title = QLabel('Spy')
         boxes_label = QLabel('$$$')
@@ -24,15 +36,21 @@ class MainWindow(QWidget):
         vbox.addWidget(score_label)
         vbox.addWidget(reset_button)
 
-        hbox.addWidget(list_widget)
+        hbox.addWidget(self.table_widget)
         hbox.addLayout(vbox)
 
         self.setLayout(hbox)
-
         self.show()
 
-    def center(self):
-        qr = self.frameGeometry()
-        cp = QDesktopWidget().availableGeometry().center()
-        qr.moveCenter(cp)
-        self.move(qr.topLeft())
+    def update_table(self):
+        for i in range(len(self.game.game_map)):
+            self.table_widget.setColumnWidth(i, 30)
+            for j in range(len(self.game.game_map[i])):
+                self.table_widget.setItem(i, j, QTableWidgetItem(str(self.game.game_map[i][j])))
+                self.table_widget.cellPressed[int, int].connect(self.cell_clicked)
+                if self.game.game_map[i][j] == '*':
+                    self.table_widget.item(i, j).setBackground(QColor(200, 0, 0))
+
+    def cell_clicked(self, r, c):
+        self.game.make_move(r, c)
+
