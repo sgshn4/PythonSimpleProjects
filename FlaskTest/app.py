5938feee5f947f1ae2bbbbdc687c1129782f2c14
sgshn4 <davidov.oleg2018@yandex.ru>
2024-05-21 10:21:09 +0300

    Init commit for Task6

--- FlaskTest/app.py
+++ FlaskTest/app.py
@@ -1,6 +1,21 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+db = SQLAlchemy(app)
+
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    intro = db.Column(db.String(300), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Article %r>' % self.id
 
 
 @app.route('/')
@@ -20,4 +35,5 @@ def user(name, id):
 
 
 if __name__ == '__main__':
+    app.app_context()
     app.run(debug=True)
