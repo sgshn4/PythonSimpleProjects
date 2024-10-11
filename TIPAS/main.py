d122c1989e3e9b41c7d8967dd2f58b438f499557
sgshn4 <davidov.oleg2018@yandex.ru>
2024-10-11 16:09:37 +0300

    Added func for calculating spectrum.

--- TIPAS/main.py
+++ TIPAS/main.py
@@ -3,28 +3,38 @@ import numpy as np
 
 points_count = 10000
 width = 10
+frequency = 2
 
 
 def calculate_coordinates_harmonic(frequency):
-    global points_count, width
+    global points_count, width, points_count
     x = np.linspace(0, width * np.pi, points_count)
     y = 2 * np.cos(frequency * x)
     return x, y
 
 
 def calculate_coordinates_digital(frequency):
-    global points_count, width
-    x = np.linspace(0, width * np.pi, 10000)
+    global points_count, width, points_count
+    x = np.linspace(0, width * np.pi, points_count)
     y = np.sign(np.cos(frequency * x))
     return x, y
 
 
-x, y = calculate_coordinates_digital(8)
+def calculate_coordinates_spectrum(frequency):
+    global points_count, width, points_count
+    signal = np.sin(2 * np.pi * frequency * np.arange(0, 1.0, 1/points_count))
+    frequency_spectrum = np.fft.rfft(signal)
+    frequencies = np.fft.rfftfreq(len(signal), d=1 / points_count)
+    return frequencies, np.abs(frequency_spectrum)
+
+
+x, y = calculate_coordinates_harmonic(frequency)
 plt.plot(x, y, marker=None, linestyle='-')
 
 
-plt.title("Пример графика")
-plt.xlabel("Амплисюда")
-plt.ylabel("Амплитуда")
+plt.title(f'Пример графика {frequency} ГЦ')
+plt.xlabel('Время, с')
+plt.ylabel('Амплитуда')
+plt.xlim(0, 30)
 plt.grid()
 plt.show()
